b8c87ffe64027792f554d66b46b3bb5d39b229e5
srihas11 <srihas.d2@gmail.com>
2023-02-12 20:44:19 +0530

    Loops

---
+++ Loops/Loops.js
@@ -0,0 +1,14 @@
+var y = 34; // A while loop is a way to repeat code until some condition is false.
+while (y < 40) {
+    console.log(y, 30, y)
+    y += 20;
+}
+
+var myFriends = ['Winston', 'OhNoesGuy', 'John', 'Sophia', 'Bale']; //Looping an Array (For loop)
+for(var x = 0; x < myFriends.length; x++)
+console.log(myFriends[x]);
+
+var y = 40; //Infinite loop
+while (y < 400) {
+    console.log(y, 30, y);
+}
