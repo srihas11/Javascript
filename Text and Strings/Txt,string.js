ab850272ab199bfcee46d741990e445363a39c91
srihas11 <srihas.d2@gmail.com>
2023-02-12 19:41:55 +0530

    Strings and text

--- Text and Strings/Txt,string.js
+++ Text and Strings/Txt,string.js
@@ -3,3 +3,9 @@ console.log("Hello World!") // Printing a string
 var myGreeting = "Hello World!"; // Store string in variable
 console.log(myGreeting) // Printing a string variable
 var myGreeting = 'Hello World!'; // Single quotes string
+var myGreeting = "Alo";
+var myName = " Winston";
+console.log(myGreeting + " ," + myName) // Concatenating strings
+var xPos = 10;
+var yPos = 20;
+console.log(xPos + " , " + yPos) //Also combine strings with numbers
